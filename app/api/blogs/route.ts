31f02e27b7a8bb9c892e6ae9618f4cf39ba1b3b5
osteensouthpaw <osteensouthpaw@gmail.com>
2024-11-14 15:19:00 +0300

    build an api for creating blogs

---
+++ app/api/blogs/route.ts
@@ -0,0 +1,32 @@
+import prisma from "@/prisma/client";
+import { Blog } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { json } from "stream/consumers";
+import { z } from "zod";
+
+const createBlogSchema = z.object({
+  title: z
+    .string()
+    .max(255)
+    .min(3, { message: "title cannot be less that 3 characters" }),
+  content: z
+    .string()
+    .min(3, { message: "content cannot be less that 3 characters" }),
+});
+
+export async function POST(request: NextRequest) {
+  const body: Blog = await request.json();
+  const validation = createBlogSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 401 });
+
+  const newBlog = await prisma.blog.create({
+    data: {
+      title: body.title,
+      content: body.content,
+    },
+  });
+
+  return NextResponse.json(newBlog, { status: 201 });
+}
