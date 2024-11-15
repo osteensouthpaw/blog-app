866ba43ba9a4cae9a0c8989457774cd84584763c
osteensouthpaw <osteensouthpaw@gmail.com>
2024-11-15 09:30:23 +0300

    handling form submission

--- app/blogs/new/page.tsx
+++ app/blogs/new/page.tsx
@@ -2,14 +2,37 @@
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { Controller, useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface BlogForm {
+  title: string;
+  content: string;
+}
 
 const NewBlogPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit, control } = useForm<BlogForm>();
+
   return (
-    <div className="max-w-xl space-y-5">
-      <TextField.Root placeholder="Title" />
-      <SimpleMDE placeholder="Content here" />
+    <form
+      className="max-w-xl space-y-5"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/blogs", data);
+        router.push("/blogs");
+      })}
+    >
+      <TextField.Root placeholder="Title" {...register("title")} />
+      <Controller
+        name="content"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Content here" {...field} />
+        )}
+      />
       <Button radius="full">Publish</Button>
-    </div>
+    </form>
   );
 };
 
