b0eee0c16c28d8bbcf3cde6ae75b37df95484ab9
osteensouthpaw <osteensouthpaw@gmail.com>
2024-11-19 10:57:30 +0300

    styling the blogs page

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,5 +1,5 @@
 import prisma from "@/prisma/client";
-import { Box, Flex, Heading, Link, Text } from "@radix-ui/themes";
+import { Box, Flex, Heading, Link, Text, Card } from "@radix-ui/themes";
 import NextLink from "next/link";
 import { RxDoubleArrowRight } from "react-icons/rx";
 
@@ -20,24 +20,31 @@ const BlogsPage = async () => {
           <Flex key={blog.id} className="flex-col md:flex-row gap-2">
             <Text
               as="p"
-              className="text-zinc-400 text-xs md:text-sm md:w-1/6 pl-3 border-l-4 md:border-none"
+              className="text-zinc-400 text-xs md:text-sm pl-3 border-l-4 md:border-none flex-shrink-0 md:w-36 py-4"
             >
               {blog.createdAt.toDateString()}
             </Text>
-            <Flex flexGrow="1" gap="4" direction="column">
-              <Heading as="h3" size="4" className="font-bold">
-                {blog.title}
-              </Heading>
-              <Text as="p" className="text-zinc-500">
-                {blog.content}
-              </Text>
-              <NextLink legacyBehavior href={`/blogs/${blog.id}`}>
-                <Link size="2">
-                  Read article
-                  <RxDoubleArrowRight size={12} className="inline-flex m-1" />
-                </Link>
-              </NextLink>
-            </Flex>
+            <NextLink href={`/blogs/${blog.id}`}>
+              <Flex
+                flexGrow="1"
+                gap="4"
+                direction="column"
+                className="hover:bg-zinc-50 transition-colors rounded-lg p-4"
+              >
+                <Heading as="h3" size="4" className="font-bold">
+                  {blog.title}
+                </Heading>
+                <Text as="p" className="text-zinc-500">
+                  {blog.content.split(" ").slice(0, 60).join(" ").concat("...")}
+                </Text>
+                <NextLink legacyBehavior href={`/blogs/${blog.id}`}>
+                  <Link size="2">
+                    Read article
+                    <RxDoubleArrowRight size={12} className="inline-flex m-1" />
+                  </Link>
+                </NextLink>
+              </Flex>
+            </NextLink>
           </Flex>
         ))}
       </Flex>
