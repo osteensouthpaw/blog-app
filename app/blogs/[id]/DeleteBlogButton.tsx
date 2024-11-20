a40512adfac5efb9c454d8c40b4e4f0fabb662a5
osteensouthpaw <osteensouthpaw@gmail.com>
2024-11-20 06:09:53 +0300

    delete an issue

--- app/blogs/[id]/DeleteBlogButton.tsx
+++ app/blogs/[id]/DeleteBlogButton.tsx
@@ -1,8 +1,12 @@
+"use client";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { BiTrash } from "react-icons/bi";
 
 const DeleteBlogButton = ({ issueId }: { issueId: number }) => {
+  const router = useRouter();
   return (
     <>
       <AlertDialog.Root>
@@ -16,9 +20,18 @@ const DeleteBlogButton = ({ issueId }: { issueId: number }) => {
           <AlertDialog.Description>
             This action cannot be reversed, are you sure you want to proceed?
           </AlertDialog.Description>
-          <Flex gap="3" mt='5'>
+          <Flex gap="3" mt="5">
             <AlertDialog.Action>
-              <Button color="red">Delete</Button>
+              <Button
+                color="red"
+                onClick={async () => {
+                  await axios.delete(`/api/blogs/${issueId}`);
+                  router.push("/blogs");
+                  router.refresh();
+                }}
+              >
+                Delete
+              </Button>
             </AlertDialog.Action>
             <AlertDialog.Cancel>
               <Button variant="soft" color="gray">
