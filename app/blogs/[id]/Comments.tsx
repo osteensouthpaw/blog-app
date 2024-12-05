dc0715bceb624b09ada6fa079f8d88da0fb92f99
osteensouthpaw <osteensouthpaw@gmail.com>
2024-12-05 15:06:51 +0300

    sort comments

--- app/blogs/[id]/Comments.tsx
+++ app/blogs/[id]/Comments.tsx
@@ -7,11 +7,14 @@ import CommentCard from "./CommentCard";
 const Comments = async ({ blog }: { blog: Blog }) => {
   const comments = await prisma.comment.findMany({
     where: { blogId: blog.id },
+    orderBy: { createdAt: "desc" },
   });
 
   return (
     <Box className="mt-8 space-y-3">
-      <Text className="font-semibold text-xl">Comments</Text>
+      <Text className="font-semibold text-xl">
+        Comments ({comments.length})
+      </Text>
       <CommentForm blogId={blog.id} />
       <Box className="space-y-5">
         {comments.map((comment) => (
